86889f01d24d3f24c33484698822939a1eba2026
ericjeevan4 <ericjeevan4@gmail.com>
2024-07-18 20:50:41 +0530

    Create model.py

---
+++ model.py
@@ -0,0 +1,14 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(150), nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
